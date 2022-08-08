26db3fb08bbc6e479f52ecea852991705e4ea5f9
Tej Patel <tejpatel317@gmail.com>
2022-08-08 19:36:13 -0400

    commit tree 4450877ac5a32c790fb9872b54fdcd5910ddd871

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validTriangle
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @c == @a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def third_length?
+    @a + @b > @c && @a + @c > @b && @b + @c > @a
+  end
+
+  def sides_over_zero?
+    @a > 0 && @b > 0 && @c > 0
+  end
+
+  def validTriangle
+    if third_length? && sides_over_zero?
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
